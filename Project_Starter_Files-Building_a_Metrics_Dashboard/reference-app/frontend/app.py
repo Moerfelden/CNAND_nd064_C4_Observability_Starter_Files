560016c955b81bb645949ec154acf139b0fc20ea
Moerfelden <96487145+Moerfelden@users.noreply.github.com>
2025-08-13 12:02:54 +0200

    Update app.py

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template, request
+from prometheus_flask_exporter import PrometheusMetrics
+import logging
 
 app = Flask(__name__)
 
+# enable the collection and exposure of metrics for your Flask application, which can be scraped by Prometheus.
+metrics = PrometheusMetrics(app) 
+# static information as metric
+metrics.info("app_info", "Application info", version="1.0.3")
+# for usage of the Prometheus Flask exporter library see https://pypi.org/project/prometheus-flask-exporter/.
+by_endpoint_counter = metrics.counter('by_endpoint_counter', 'Request count by endpoints', labels={'endpoint': lambda: request.endpoint})
+by_path_counter = metrics.counter('by_path_counter', 'Request count by request paths', labels={'path': lambda: request.path})
 
-@app.route("/")
+logging.getLogger('').handlers = []
+logging.basicConfig(format = '%(message)s', level = logging.INFO)
+
+@app.route('/')
+@by_endpoint_counter
+@by_path_counter
 def homepage():
-    return render_template("main.html")
-
+    app.logger.info('Accessing the frontend homepage')
+    try:
+        return render_template("main.html")
+    except Exception as e:
+        print(f"Error occurred: {e}")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
